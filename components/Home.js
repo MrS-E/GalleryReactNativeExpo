6c03e2128b71f998c62e7ac652ffaa3f209d310c
MrS-E <simeon.stix@gmx.ch>
2023-10-04 04:54:08 +0200

    arbeit

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,18 @@
 import React, {useState} from "react";
 import {useAssets} from "expo-asset";
-import {Image, ImageBackground, View} from "react-native";
-import styles from "./styles";
+import {Image, ImageBackground, StyleSheet, View} from "react-native";
 import GestureRecognizer from "react-native-swipe-gestures";
 import {IconButton} from "react-native-paper";
 import Details from "./HomeDetails";
+import * as SQLite from 'expo-sqlite';
 
 export default function ({ navigation }) {
-    const [images, setImages] = useState([{uri:"https://platinumlist.net/guide/wp-content/uploads/2023/03/IMG-worlds-of-adventure.webp"},{uri:"https://cdn.cookielaw.org/logos/889c435d-64b4-46d8-ad05-06332fe1d097/4353a07c-5b48-453a-b5ab-e8498c172697/IMG-ReBrand-Blue.png"},{uri:"https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/16:9/w_1280,c_limit/IMG%20Worlds%20of%20Adventure%20-%201.jpg"}])
+    const [images, setImages] = useState(require('../assets/test_data.json'))
     const [id, setId] = useState(0)
     const [showDetails, setShowDetails] = useState(false)
     const [showLike, setShowLike] = useState()
     const [assets, error] = useAssets([require('../assets/icons/love.svg'), require('../assets/icons/share.svg'),require('../assets/icons/comment.svg'),require('../assets/icons/add.svg')]);
+
 
     //swipe handler
     const swipeUp = () => {
@@ -32,6 +33,7 @@ export default function ({ navigation }) {
     }
     //button handler
 
+    {console.log(images[id])}
     return (
         <View style={styles.container}>
             <GestureRecognizer style={{flex:1}} onSwipeUp={swipeUp} onSwipeDown={swipeDown} onSwipeLeft={()=>setShowDetails(true)}>
@@ -46,10 +48,40 @@ export default function ({ navigation }) {
                 <IconButton style={styles.control_btn} icon={assets ? assets[2] : "comment"} />*/}
                 <IconButton style={styles.control_btn} icon={assets ? assets[3] : "plus"} onPress={()=>navigation.navigate('New')} />
             </View>
-
-            <Details id={images[id]} setShowDetails={setShowDetails} showDetails={showDetails}/>
+            <Details post={images[id]} setShowDetails={setShowDetails} showDetails={showDetails}/>
 
 
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        //paddingTop: Platform.OS === 'ios'? 0 : StatusBar.currentHeight
+    },
+    controlles:{
+        position: "absolute",
+        width: 40,
+        top: "25%",
+        height:"50%",
+        right:"3%",
+        display: "flex",
+        justifyContent:"center",
+        flexDirection: "column"
+    },
+    control_btn:{
+        backgroundColor: "#fff"
+    },
+    image_container:{
+        display: "flex",
+        flex:1,
+        flexDirection:"column",
+        resizeMode:"stretch"
+    },
+    image:{
+        resizeMode: "contain",
+        flex:1,
+    },
+});
--- components/HomeDetails.js
+++ components/HomeDetails.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useRef, useState} from 'react';
-import styles from "./styles";
-import {View, Image, ImageBackground, Animated, Dimensions} from 'react-native';
+import {Animated, Dimensions, View, Text, SafeAreaView, Platform, StatusBar} from 'react-native';
 import GestureRecognizer from "react-native-swipe-gestures";
+import {StyleSheet} from "react-native";
+import {Title} from "react-native-paper";
 
 
 export default function ({post, setShowDetails, showDetails}) {
@@ -48,11 +49,51 @@ export default function ({post, setShowDetails, showDetails}) {
         ]
     }
 
+    console.log(post)
+
     return show?(
         <GestureRecognizer onSwipeRight={swipeRight} style={{width:'100%',height:'100%',position:"absolute"}}>
             <Animated.View style={[styles.info_box,style]}>
-
+                <SafeAreaView style={styles.box}>
+                    <Title style={styles.title}>Details</Title>
+                    <View>
+                        <Text style={[{fontWeight:"bold"}, styles.text]}>{post.name?post.name:<Text style={{fontStyle: "italic"}}>unknown</Text>}</Text>
+                        {post.tags?<Text style={[{fontStyle: "italic"},styles.smalltext]}>{post.tags.map(v=>"#"+v+" ")}</Text>:""}
+                        {post.desc?<Text style={styles.smalltext}>{post.desc}</Text>:""}
+                        <Text style={styles.smalltext}>at the: {post.date?post.date:<Text style={{fontStyle: "italic"}}>unknown</Text>}</Text>
+                        <Text style={styles.smalltext}>from: {post.author?post.author:<Text style={{fontStyle: "italic"}}>unknown</Text>}</Text>
+                    </View>
+                </SafeAreaView>
             </Animated.View>
         </GestureRecognizer>
     ):null;
 }
+
+const styles = StyleSheet.create({
+    info_box:{
+        position: 'absolute',
+        backgroundColor: '#fff',
+        height:'100%',
+        width:'75%',
+        right: '-75%'
+    },
+    box:{
+        flex:1,
+        paddingTop: Platform.OS === 'ios'? 0 : StatusBar.currentHeight,
+        paddingLeft: "5%"
+    },
+    title:{
+        fontWeight: "bold",
+        fontSize: 30,
+        paddingBottom:"5%"
+    },
+    text:{
+        paddingTop:"2%",
+        paddingBottom:"2%",
+        fontSize: 20
+    },
+    smalltext:{
+        paddingTop:"1.5%",
+        fontSize: 18
+    }
+})
